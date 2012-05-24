d38e0b8eb9358a4c6c019f29cb1f09c4ad36d6d0
Dario Seminara <darios3@gmail.com>
2012-05-24 13:01:52 -0300

    +10 failing tests: parsing of numeric atoms with spaces around

--- spec/ast.rb
+++ spec/ast.rb
@@ -8,8 +8,14 @@ describe DsLisp, "ds lisp"  do
     end 
   end
 
+  (1..10).each do |i|
+    it "should parse simple numeric atom #{i} with spaces around" do
+      DsLisp.new.parse("  " + i.to_s + "   ").should be == i
+    end 
+  end
+
   [:x,:y,:z].each do |symbol|
-    it "should parse simple symbolic #{symbol}" do
+    it "should parse simple symbolic atom #{symbol}" do
       DsLisp.new.parse(symbol.to_s).should be == symbol
     end 
   end
