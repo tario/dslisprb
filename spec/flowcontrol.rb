4b130b91af834098a084ac1d10c037564c25f8c8
Dario Seminara <darios3@gmail.com>
2012-05-26 18:32:08 -0300

    +2 failing test: cond statement

--- spec/flowcontrol.rb
+++ spec/flowcontrol.rb
@@ -11,6 +11,14 @@ describe DsLisp, "ds lisp flow control"  do
 
   it "should return nil when first statement return false and there is no second statement" do
     DsLisp.new.evaluate([:if, :nil, [:+, 110, 1]]).should be == nil
+  end
+
+  it "should execute cond statement" do
+    DsLisp.new.evaluate([:cond, [:T, [:+, 120, 3]]]).should be == 123
+  end 
+
+  it "should execute second statement of cond if the first fail" do
+    DsLisp.new.evaluate([:cond, [:nil, [:+, 120, 3]], [:T, [:+, 120, 5]]]).should be == 125
   end 
 end
 
