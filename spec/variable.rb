f61d05637f7cd065f89a8268a06cc0455b837ca6
Dario Seminara <darios3@gmail.com>
2013-01-02 10:53:39 -0300

    +2 failing test: variable assignment via let

--- spec/variable.rb
+++ spec/variable.rb
@@ -4,5 +4,13 @@ describe DsLisp, "ds lisp variables"  do
   it "should evaluate variables on if" do
     DsLisp.new.evaluate("((lambda (x) (if (> x 2) x 2)) 3)").should be == 3
   end
+
+  it "should assign variables via let" do
+    DsLisp.new.evaluate("(let ((a 5)) a)").should be == 5
+  end
+
+  it "should assign multiple variables via let" do
+    DsLisp.new.evaluate("(let ((a 5) (b 6)) (+ a b) )").should be == 11
+  end
 end
 
