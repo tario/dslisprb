a6018bca6928013f0ccb3f22320eb8e61d9e8bd6
Dario Seminara <darios3@gmail.com>
2013-01-04 21:11:56 -0300

    +3 failing test: cadr, cdar, cddr

--- spec/caadr.rb
+++ spec/caadr.rb
@@ -3,11 +3,14 @@ require "dslisprb"
 describe DsLisp, "ds lisp"  do
   def self.test_func(a,b,value)
     it "(#{a} #{value}) should be equivalent to (#{b} #{value}#{")"*b.count("(")})" do
-      DsLisp.new.evaluate("(caar '#{value})").should be == DsLisp.new.evaluate("(#{b} '#{value}#{")"*b.count("(")})")
+      DsLisp.new.evaluate("(#{a} '#{value})").should be == DsLisp.new.evaluate("(#{b} '#{value}#{")"*b.count("(")})")
     end 
   end
 
   test_func("caar","car ( car", "((1 2) (3 4))")
+  test_func("cadr","car ( cdr", "((1 2) (3 4))")
+  test_func("cdar","cdr ( car", "((1 2) (3 4))")
+  test_func("cddr","cdr ( cdr", "((1 2) (3 4))")
 
 end
 
