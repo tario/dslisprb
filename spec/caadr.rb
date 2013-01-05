209e00df1002a2d31870e32acd997980bac548cb
Dario Seminara <darios3@gmail.com>
2013-01-04 21:15:26 -0300

    +8 failing test: caaar, caadr, cadar, etc...

--- spec/caadr.rb
+++ spec/caadr.rb
@@ -12,5 +12,15 @@ describe DsLisp, "ds lisp"  do
   test_func("cdar","cdr ( car", "((1 2) (3 4))")
   test_func("cddr","cdr ( cdr", "((1 2) (3 4))")
 
+  test_value_3 = "(((1 2) (3 4)) ((5 6) (7 8)))"
+  test_func("caaar","car ( car ( car", test_value_3)
+  test_func("caadr","car ( car ( cdr", test_value_3)
+  test_func("cadar","car ( cdr ( car", test_value_3)
+  test_func("caddr","car ( cdr ( cdr", test_value_3)
+  test_func("cdaar","cdr ( car ( car", test_value_3)
+  test_func("cdadr","cdr ( car ( cdr", test_value_3)
+  test_func("cddar","cdr ( cdr ( car", test_value_3)
+  test_func("cdddr","cdr ( cdr ( cdr", test_value_3)
+
 end
 
